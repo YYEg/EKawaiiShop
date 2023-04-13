a1a379ce982bd8d462370e8035ceac5c9def5961
Egor <yyrkovskii@mail.ru>
2023-04-13 21:48:38 +0800

    register page design and logic

--- app/src/main/java/com/yy/ekawaiishop/LoginActivity.java
+++ app/src/main/java/com/yy/ekawaiishop/LoginActivity.java
@@ -1,14 +1,122 @@
 package com.yy.ekawaiishop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextLoginEmail, editTextLoginPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        findViews();
+        showHidePassword();
+
+        TextView textViewRegister = findViewById(R.id.textViewLoginSignUp);
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerActivity = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(registerActivity);
+            }
+        });
+
+        auth = FirebaseAuth.getInstance();
+
+        //пользователь логиниться
+        Button buttonLogin = findViewById(R.id.btnLoginLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textEmail = editTextLoginEmail.getText().toString();
+                String textPwd = editTextLoginPassword.getText().toString();
+
+                //check if all the data are present before signin in user
+                if (TextUtils.isEmpty(textEmail)){
+                    Toast.makeText(LoginActivity.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("email is required");
+                    editTextLoginEmail.requestFocus();
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()){
+                    Toast.makeText(LoginActivity.this, "Please re-enter your email", Toast.LENGTH_SHORT).show();
+                    editTextLoginEmail.setError("Valid email is required");
+                    editTextLoginEmail.requestFocus();
+                } else if (TextUtils.isEmpty(textPwd)){
+                    Toast.makeText(LoginActivity.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                    editTextLoginPassword.setError("password is required");
+                    editTextLoginPassword.requestFocus();
+                } else{
+                    progressBar.setVisibility(View.VISIBLE);
+                    loginUser(textEmail, textPwd);
+                }
+            }
+        });
+    }
+
+    private void loginUser(String textEmail, String textPwd) {
+        auth.signInWithEmailAndPassword(textEmail, textPwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    Intent UserProfileActivity = new Intent(LoginActivity.this, UserProfileActivity.class);
+                    startActivity(UserProfileActivity);
+                } else{
+                    try {
+                        throw task.getException();
+                    } catch (Exception e){
+                        Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    private void showHidePassword() {
+        ImageView imageViewShowHidePwd = findViewById(R.id.imageView_show_hide_pwd);
+        imageViewShowHidePwd.setImageResource(R.drawable.ic_show_pwd);
+
+        imageViewShowHidePwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editTextLoginPassword.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance())){
+                    editTextLoginPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    imageViewShowHidePwd.setImageResource(R.drawable.ic_show_pwd);
+                } else {
+                    editTextLoginPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    imageViewShowHidePwd.setImageResource(R.drawable.ic_hide_pwd);
+                }
+            }
+        });
+    }
+
+    private void findViews() {
+        editTextLoginEmail = findViewById(R.id.editTextLoginPageEmail);
+        editTextLoginPassword = findViewById(R.id.editTextLoginPagePassword);
+        progressBar = findViewById(R.id.progressBar);
     }
 }
---
+++ app/src/main/java/com/yy/ekawaiishop/RegisterActivity.java
@@ -0,0 +1,136 @@
+package com.yy.ekawaiishop;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editTextRegisterEmail, editTextRegisterName, editTextRegisterPassword;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        findViews();
+        showHidePassword();
+
+        Button ButtonRegister = findViewById(R.id.btnRegisterPageRegister);
+        ButtonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textEmail = editTextRegisterEmail.getText().toString();
+                String textPwd = editTextRegisterPassword.getText().toString();
+                String textName = editTextRegisterName.getText().toString();
+
+                if (TextUtils.isEmpty(textName)){
+                    Toast.makeText(RegisterActivity.this, "Please enter your name", Toast.LENGTH_SHORT).show();
+                    editTextRegisterName.setError("Name is required");
+                    editTextRegisterName.requestFocus();
+                } else if (TextUtils.isEmpty(textEmail)){
+                    Toast.makeText(RegisterActivity.this, "Please enter your email", Toast.LENGTH_SHORT).show();
+                    editTextRegisterEmail.setError("email is required");
+                    editTextRegisterEmail.requestFocus();
+                } else if (!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()){
+                    Toast.makeText(RegisterActivity.this, "Please re-enter your email", Toast.LENGTH_SHORT).show();
+                    editTextRegisterEmail.setError("Valid email is required");
+                    editTextRegisterEmail.requestFocus();
+                } else if (TextUtils.isEmpty(textPwd)){
+                    Toast.makeText(RegisterActivity.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+                    editTextRegisterPassword.setError("password is required");
+                    editTextRegisterPassword.requestFocus();
+                } else if (textPwd.length() < 6){
+                    Toast.makeText(RegisterActivity.this, "password too short!", Toast.LENGTH_SHORT).show();
+                    editTextRegisterPassword.setError("Password should be at least 6 digits");
+                    editTextRegisterPassword.requestFocus();
+                } else{
+                    progressBar.setVisibility(View.VISIBLE);
+                    registerUser(textEmail, textName, textPwd);
+                }
+            }
+        });
+    }
+
+
+    private void showHidePassword() {
+        ImageView imageViewShowHidePwd = findViewById(R.id.imageView_show_hide_pwd);
+        imageViewShowHidePwd.setImageResource(R.drawable.ic_show_pwd);
+
+        imageViewShowHidePwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (editTextRegisterPassword.getTransformationMethod().equals(HideReturnsTransformationMethod.getInstance())){
+                    editTextRegisterPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    imageViewShowHidePwd.setImageResource(R.drawable.ic_show_pwd);
+                } else {
+                    editTextRegisterPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    imageViewShowHidePwd.setImageResource(R.drawable.ic_hide_pwd);
+                }
+            }
+        });
+    }
+
+    private void findViews() {
+        editTextRegisterEmail = findViewById(R.id.editTextRegisterPageEmail);
+        editTextRegisterPassword = findViewById(R.id.editTextRegisterPagePassword);
+        editTextRegisterName = findViewById(R.id.editTextRegisterPageName);
+        progressBar = findViewById(R.id.progressBar);
+    }
+
+    private void registerUser(String textEmail, String textName, String textPwd) {
+        auth = FirebaseAuth.getInstance();
+        auth.createUserWithEmailAndPassword(textEmail, textPwd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                //Check iff the user creation is succesful
+                if(task.isSuccessful()){
+                    FirebaseUser firebaseUser = auth.getCurrentUser();
+                    if(firebaseUser != null){
+                        //Update display name of the user
+                        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder().
+                                setDisplayName(textName).build();
+                        firebaseUser.updateProfile(profileUpdates);
+                        Toast.makeText(RegisterActivity.this, "Registration Successful!", Toast.LENGTH_SHORT).show();
+                        //open userprofileacivity after user is created
+                        Intent UserProfileActivity = new Intent(RegisterActivity.this, UserProfileActivity.class);
+
+                        //Stop user from going back to Register Activity on pressing back button
+                        UserProfileActivity.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(UserProfileActivity);
+                        finish();
+                    }
+                } else {
+                    try {
+                        throw task.getException();
+                    } catch (Exception e) {
+                        Toast.makeText(RegisterActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+    }
+
+}
