ee4d09ec58d3e1c5274e10f28136bdbcd5491432
Egor <yyrkovskii@mail.ru>
2023-04-13 20:34:57 +0800

    Start Page logic

--- app/src/main/java/com/yy/ekawaiishop/MainActivity.java
+++ app/src/main/java/com/yy/ekawaiishop/MainActivity.java
@@ -2,13 +2,35 @@ package com.yy.ekawaiishop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_app);
+
+        progressBar = findViewById(R.id.progressBar);
+
+        //Кнопка старт
+        Button btnStart = findViewById(R.id.btnStartPageStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                Toast.makeText(MainActivity.this, "Welcome!", Toast.LENGTH_SHORT).show();
+                Intent LoginActivity = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(LoginActivity);
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
     }
 }
