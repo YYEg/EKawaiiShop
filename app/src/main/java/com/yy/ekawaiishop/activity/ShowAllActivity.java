958612a4721fa401aabd6b0804d0bf4f1515e679
Egor <yyrkovskii@mail.ru>
2023-04-19 21:34:45 +0800

    see all added(not over maybe)

--- app/src/main/java/com/yy/ekawaiishop/activity/ShowAllActivity.java
+++ app/src/main/java/com/yy/ekawaiishop/activity/ShowAllActivity.java
@@ -1,16 +1,55 @@
 package com.yy.ekawaiishop.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.yy.ekawaiishop.R;
+import com.yy.ekawaiishop.adapters.ShowAllAdapter;
+import com.yy.ekawaiishop.models.ShowAllModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShowAllActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ShowAllAdapter showAllAdapter;
+    List<ShowAllModel> showAllModelList;
+
+    FirebaseFirestore firestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_all);
+
+        firestore = FirebaseFirestore.getInstance();
+        recyclerView = findViewById(R.id.show_all_rec);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        showAllModelList = new ArrayList<>();
+        showAllAdapter = new ShowAllAdapter(this, showAllModelList);
+        recyclerView.setAdapter(showAllAdapter);
+
+        firestore.collection("ShowAll")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        for (DocumentSnapshot doc :task.getResult().getDocuments()){
+                            ShowAllModel showAllModel = doc.toObject(ShowAllModel.class);
+                            showAllModelList.add(showAllModel);
+                            showAllAdapter.notifyDataSetChanged();
+                        }
+                    }
+                });
     }
 }
