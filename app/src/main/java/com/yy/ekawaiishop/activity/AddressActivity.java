a31d01dfe5a5ba7a1135c9f1522112d548c359a5
Egor <yyrkovskii@mail.ru>
2023-04-24 01:08:07 +0800

    address choose is now working

--- app/src/main/java/com/yy/ekawaiishop/activity/AddressActivity.java
+++ app/src/main/java/com/yy/ekawaiishop/activity/AddressActivity.java
@@ -1,29 +1,95 @@
 package com.yy.ekawaiishop.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.yy.ekawaiishop.R;
+import com.yy.ekawaiishop.adapters.AddressAdapter;
+import com.yy.ekawaiishop.models.AddressModel;
+import com.yy.ekawaiishop.models.MyCartModel;
 
-public class AddressActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressActivity extends AppCompatActivity implements AddressAdapter.ISelectedAddress {
 
 
-    Button addAddress;
+    Button addAddress, paymentButton;
+    RecyclerView recyclerView;
+    private List<AddressModel> addressModelList;
+    private AddressAdapter addressAdapter;
+    FirebaseFirestore firestore;
+    FirebaseAuth auth;
+    Toolbar toolbar;
+    String mAddress = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addres);
 
+        toolbar = findViewById(R.id.address_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firestore = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        recyclerView = findViewById(R.id.address_recycler);
+
+        paymentButton = findViewById(R.id.payment_btn);
         addAddress = findViewById(R.id.add_address_btn);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        addressModelList = new ArrayList<>();
+        addressAdapter = new AddressAdapter(getApplicationContext(), addressModelList,this);
+        recyclerView.setAdapter(addressAdapter);
+
+        firestore.collection("CurrentUser").document(auth.getCurrentUser().getUid())
+                        .collection("Address").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            for (DocumentSnapshot doc :task.getResult().getDocuments()){
+                                AddressModel addressModel = doc.toObject(AddressModel.class);
+                                addressModelList.add(addressModel);
+                                addressAdapter.notifyDataSetChanged();
+                            }
+                        }
+                    }
+                });
+        paymentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(AddressActivity.this, PaymentActivity.class));
+            }
+        });
+
+
         addAddress.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(AddressActivity.this, AddAddressActivity.class));
             }
         });
+    }
+
+    @Override
+    public void setAddress(String address) {
+
+        mAddress = address;
     }
 }
---
+++ app/src/main/java/com/yy/ekawaiishop/activity/PaymentActivity.java
@@ -0,0 +1,16 @@
+package com.yy.ekawaiishop.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.yy.ekawaiishop.R;
+
+public class PaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+    }
+}
